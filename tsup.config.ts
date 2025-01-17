f5dc4cde1f534da8192387edd0d965a3d1805df0
Davit Petrosyan <7922751+davitp@users.noreply.github.com>
2025-01-17 19:08:13 +0400

    Disable source map

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   dts: true,
   entry: ["src/index.ts"],
   format: ["esm"],
-  sourcemap: true,
+  sourcemap: false,
   minify: true,
   target: "node20",
   platform: "node",
